fc30406544d6b41603f8a9d323e8ca1b22020607
nallaperumal007 <nallaperumal342@gmail.com>
2024-07-10 16:41:43 +0530

    finish

---
+++ src/EmailEditor.tsx
@@ -0,0 +1,107 @@
+import React, {
+  useEffect,
+  useState,
+  useImperativeHandle,
+  useMemo,
+} from 'react';
+
+import pkg from '../package.json';
+import { Editor, EditorRef, EmailEditorProps } from './types';
+import { loadScript } from './loadScript';
+
+const win = typeof window === 'undefined' ? { __unlayer_lastEditorId: 0 } : window
+win.__unlayer_lastEditorId = win.__unlayer_lastEditorId || 0;
+
+export const EmailEditor = React.forwardRef<EditorRef, EmailEditorProps>(
+  (props, ref) => {
+    const { onLoad, onReady, scriptUrl, minHeight = 500, style = {} } = props;
+
+    const [editor, setEditor] = useState<Editor | null>(null);
+
+    const [hasLoadedEmbedScript, setHasLoadedEmbedScript] = useState(false);
+
+    const editorId = useMemo(
+      () => props.editorId || `editor-${++win.__unlayer_lastEditorId}`,
+      [props.editorId]
+    );
+
+    const options: EmailEditorProps['options'] = {
+      ...(props.options || {}),
+      appearance: props.appearance ?? props.options?.appearance,
+      displayMode: props?.displayMode || props.options?.displayMode || 'email',
+      locale: props.locale ?? props.options?.locale,
+      projectId: props.projectId ?? props.options?.projectId,
+      tools: props.tools ?? props.options?.tools,
+
+      id: editorId,
+      source: {
+        name: pkg.name,
+        version: pkg.version,
+      },
+    };
+
+    useImperativeHandle(
+      ref,
+      () => ({
+        editor,
+      }),
+      [editor]
+    );
+
+    useEffect(() => {
+      return () => {
+        editor?.destroy();
+      };
+    }, []);
+
+    useEffect(() => {
+      setHasLoadedEmbedScript(false);
+      loadScript(() => setHasLoadedEmbedScript(true), scriptUrl);
+    }, [scriptUrl]);
+
+    useEffect(() => {
+      if (!hasLoadedEmbedScript) return;
+      editor?.destroy();
+      setEditor(unlayer.createEditor(options));
+    }, [JSON.stringify(options), hasLoadedEmbedScript]);
+
+    const methodProps = Object.keys(props).filter((propName) =>
+      /^on/.test(propName)
+    );
+    useEffect(() => {
+      if (!editor) return;
+
+      onLoad?.(editor);
+
+      // All properties starting with on[Name] are registered as event listeners.
+      methodProps.forEach((methodProp) => {
+        if (
+          /^on/.test(methodProp) &&
+          methodProp !== 'onLoad' &&
+          methodProp !== 'onReady' &&
+          typeof props[methodProp] === 'function'
+        ) {
+          editor.addEventListener(methodProp, props[methodProp]);
+        }
+      });
+
+      if (onReady) {
+        editor.addEventListener('editor:ready', () => {
+          onReady(editor);
+        });
+      }
+    }, [editor, Object.keys(methodProps).join(',')]);
+
+    return (
+      <div
+        style={{
+          flex: 1,
+          display: 'flex',
+          minHeight: minHeight,
+        }}
+      >
+        <div id={editorId} style={{ ...style, flex: 1 }} />
+      </div>
+    );
+  }
+);
---
+++ src/types.ts
@@ -0,0 +1,45 @@
+/// <reference path="../node_modules/unlayer-types/embed.d.ts" />
+
+import { CSSProperties } from 'react';
+
+import Embed from 'embed/index';
+import { Editor as EditorClass } from 'embed/Editor';
+import { AppearanceConfig, DisplayMode, ToolsConfig } from 'state/types/types';
+
+export type Unlayer = typeof Embed;
+export type UnlayerOptions = Parameters<Unlayer['createEditor']>[0];
+export type Editor = InstanceType<typeof EditorClass>;
+
+export interface EditorRef {
+  editor: Editor | null;
+}
+
+export interface EmailEditorProps {
+  editorId?: string | undefined;
+  minHeight?: number | string | undefined;
+  onLoad?(unlayer: Editor): void;
+  onReady?(unlayer: Editor): void;
+  options?: UnlayerOptions | undefined;
+  scriptUrl?: string | undefined;
+  style?: CSSProperties | undefined;
+
+  // redundant props -- already available in options
+  /** @deprecated */
+  appearance?: AppearanceConfig | undefined;
+  /** @deprecated */
+  displayMode?: DisplayMode;
+  /** @deprecated */
+  locale?: string | undefined;
+  /** @deprecated */
+  projectId?: number | undefined;
+  /** @deprecated */
+  tools?: ToolsConfig | undefined;
+}
+
+declare global {
+  const unlayer: Unlayer;
+
+  interface Window {
+    __unlayer_lastEditorId: number;
+  }
+}
